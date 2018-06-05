9fcbbc8bf4c1555cfa9efd39eda620356e09285d
tinhnt <tinh.nt@anlab.info>
2018-06-05 14:27:58 +0700

    Temporarily remove transfer fee

--- contracts/token/BMCToken.sol
+++ contracts/token/BMCToken.sol
@@ -1,12 +1,10 @@
 pragma solidity ^0.4.20;
 
-import 'zeppelin-solidity/contracts/math/SafeMath.sol';
 import 'zeppelin-solidity/contracts/ownership/HasNoTokens.sol';
-import 'zeppelin-solidity/contracts/token/ERC20/MintableToken.sol';
 import 'zeppelin-solidity/contracts/token/ERC20/PausableToken.sol';
 
 import '../ownership/ClaimableEx.sol';
-import '../utils/AddressSet.sol';
+import './TraceableToken.sol';
 
 
 /**
@@ -14,15 +12,12 @@ import '../utils/AddressSet.sol';
  * @dev BMC is a ERC20 token that:
  *  - caps total number at 10 billion tokens.
  *  - can pause and unpause token transfer (and authorization) actions.
- *  - mints new tokens when purchased (rather than transferring tokens pre-granted to a holding account).
+ *  - mints new tokens when purchased.
  *  - can run a loop through all token holders.
- *  - transfers cost fee, all fees will be sent to the BitMile platform wallet.
  *  - attempts to reject ERC20 token transfers to itself and allows token transfer out.
  *  - allows the new owner to accept the ownership transfer, the owner can cancel the transfer if needed.
  **/
-contract BMCToken is HasNoTokens, MintableToken, PausableToken, ClaimableEx {
-  using SafeMath for uint256;
-
+contract BMCToken is HasNoTokens, TraceableToken, PausableToken, ClaimableEx {
   string public constant name = "BMCToken";
   string public constant symbol = "BMC";
 
@@ -31,51 +26,13 @@ contract BMCToken is HasNoTokens, MintableToken, PausableToken, ClaimableEx {
   uint256 public constant BILLION_TOKENS = (10**9) * ONE_TOKENS;
   uint256 public constant TOTAL_TOKENS = 10 * BILLION_TOKENS;
 
-  AddressSet public holderSet;
-
-  address public bitmileWallet;
-  uint8 public transferFeeRate;
-  AddressSet public freeChargeSet;
-
-  /**
-   * @param _bitmileWallet Wallet address of the BitMile platform.
-   * @param _transferFeeRate Transfer fee rate.
-   */
-  function BMCToken(address _bitmileWallet, uint8 _transferFeeRate)
+  function BMCToken()
   HasNoTokens()
-  MintableToken()
+  TraceableToken()
   PausableToken()
   ClaimableEx()
   public {
-    require(_bitmileWallet != 0x0);
-    bitmileWallet = _bitmileWallet;
 
-    require(_transferFeeRate < 100);
-    transferFeeRate = _transferFeeRate;
-
-    freeChargeSet = new AddressSet();
-    holderSet = new AddressSet();
-  }
-
-  /**
-   * @dev Changes wallet address of the BitMile platform.
-   * @param _addr A new wallet address to update.
-   */
-  function setBitMileWallet(address _addr) external onlyOwner {
-    require(_addr != 0x0);
-    require(_addr != bitmileWallet);
-
-    bitmileWallet = _addr;
-  }
-
-  /**
-   * @dev Changes transfer fee rate.
-   * @param _newRate A new rate to update.
-   */
-  function setTransferFeeRate(uint8 _newRate) external onlyOwner {
-    require(_newRate < 100);
-
-    transferFeeRate = _newRate;
   }
 
   /**
@@ -85,10 +42,7 @@ contract BMCToken is HasNoTokens, MintableToken, PausableToken, ClaimableEx {
    */
   function mint(address _to, uint256 _amount) onlyOwner canMint public returns(bool) {
     require(totalSupply_.add(_amount) <= TOTAL_TOKENS);
-    bool suc = super.mint(_to, _amount);
-    if (suc) holderSet.add(_to);
-
-    return suc;
+    return super.mint(_to, _amount);
   }
 
   /**
@@ -99,83 +53,5 @@ contract BMCToken is HasNoTokens, MintableToken, PausableToken, ClaimableEx {
     // do not allow self ownership
     require(_newOwner != address(this));
     super.transferOwnership(_newOwner);
-  }
-
-  function transfer(address _to, uint256 _value) public returns(bool) {
-    _checkTransferTarget(_to);
-
-    if (!shouldBeFree(msg.sender)) {
-      uint256 _fee = _calcTransferFee(_value);
-      super.transfer(bitmileWallet, _fee);
-    }
-
-    super.transfer(_to, _value);
-    return true;
-  }
-
-  function transferFrom(address _from, address _to, uint256 _value) public returns(bool) {
-    _checkTransferTarget(_to);
-
-    if (!shouldBeFree(_from)) {
-      uint256 _fee = _calcTransferFee(_value);
-      super.transferFrom(_from, bitmileWallet, _fee);
-    }
-
-    super.transferFrom(_from, _to, _value);
-    return true;
-  }
-
-  function estimateTransferDebit(uint256 _transferValue) public view returns(uint256) {
-    if (shouldBeFree(msg.sender)) {
-      return _transferValue;
-    }
-
-    uint256 _fee = _calcTransferFee(_transferValue);
-    return _fee.add(_transferValue);
-  }
-
-  function estimateTransferValue(uint256 _transferDebit) public view returns(uint256) {
-    if (shouldBeFree(msg.sender)) {
-      return _transferDebit;
-    }
-
-    return _transferDebit.mul(100).div(100 + transferFeeRate);
-  }
-
-  function addToFreeChargeSet(address _addr) onlyOwner public {
-    freeChargeSet.add(_addr);
-  }
-
-  function shouldBeFree(address _from) public view returns(bool) {
-    return freeChargeSet.contains(_from);
-  }
-
-  function getTheNumberOfHolders() onlyOwner external view returns(uint256) {
-    return holderSet.getTheNumberOfElements();
-  }
-
-  function getHolder(uint256 _index) onlyOwner external view returns(address) {
-    return holderSet.elementAt(_index);
-  }
-
-  function getTheNumberOfFreeSenders() onlyOwner external view returns(uint256) {
-    return freeChargeSet.getTheNumberOfElements();
-  }
-
-  function getFreeSender(uint256 _index) onlyOwner external view returns(address) {
-    return freeChargeSet.elementAt(_index);
-  }
-
-  function _checkTransferTarget(address _to) internal {
-    if (!holderSet.contains(_to)) {
-      holderSet.add(_to);
-    }
-  }
-
-  function _calcTransferFee(uint256 _transferValue) internal view returns(uint256) {
-    uint256 _fee = _transferValue.mul(transferFeeRate).div(100);
-    require(_fee > 0);
-
-    return _fee;
   }
 }
